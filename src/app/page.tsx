bc6acbb6a263cc8e7ae5e7adc5457d88b9513fb1
Aditya Sharma <3008.aditya.sharma@gmail.com>
2024-12-31 00:47:27 +0530

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <TestimonialCards />
         <UpcomingWebinars />
         <Instructors />
+        <Footer />
       </main>
     </>
   );
