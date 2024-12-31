c9df760f4c4f7e162e1959b541d0a1b645aa582e
Aditya Sharma <3008.aditya.sharma@gmail.com>
2024-12-31 12:40:08 +0530

    add courses, content page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ function Navbar({ className }: { className?: string }) {
 						<HoveredLink href="/courses">Music Production</HoveredLink>
 					</div>
 				</MenuItem>
-        <Link href={"/"}>
+        <Link href={"/contact"}>
 					<MenuItem setActive={setActive} active={active} item="Contact Us">
 					</MenuItem>
 				</Link>
